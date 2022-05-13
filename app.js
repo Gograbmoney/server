539ff5fcd25e603a073d0df6921a8818bc0b1623
thewhitewolf09 <sumitramprakashnirmal@gmail.com>
2022-05-13 23:49:03 +0530

    responsive updated

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const cookieParser = require('cookie-parser')
 const cors=require("cors");
 const app = express() 
 const corsOptions ={ 
-   origin:'https://gograbmoney.com',  
+   origin:'https://gograbmoney.com',  //https://gograbmoney.com
    credentials:true,            //access-control-allow-credentials:true
    optionSuccessStatus:200,
 }
-
+ 
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
 
--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+//DATABASE =  mongodb+srv://nrmlsumit:Sumit210%40@cluster0.hahwy.mongodb.net/gograbmoney?retryWrites=true&w=majority
  const DB = process.env.DATABASE
 
  mongoose.connect(DB).then(()=>{
