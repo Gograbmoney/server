929e822431e8149201fa830f389a700a7ac1b683
thewhitewolf09 <sumitramprakashnirmal@gmail.com>
2022-06-05 18:33:53 +0530

    merchant and offer  api update

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ const cors = require("cors");
 const app = express()
 const corsOptions = {
     origin: 'https://www.gograbmoney.com',  //https://www.gograbmoney.com & http://localhost:3000         
-    credentials : true,
-    optionsSuccessStatus: 200
+    credentials : true
 }
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
 // app.use((req, res, next) => {
-//     res.header({"Access-Control-Allow-Credentials": "true"});
+//     res.header({"Access-Control-Allow-Credentials": true);
 //     next();
 //   }) 
 
