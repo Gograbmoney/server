280bbf8fb229ebe89e3d00ca5d671e069981c866
thewhitewolf09 <sumitramprakashnirmal@gmail.com>
2022-06-09 15:16:01 +0530

    merchant and offer  api update

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ const mongoose = require('mongoose')
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require("cors");
-const app = express();
-
-const whitelist = ['https://www.gograbmoney.com', 'https://gograbmoney.com',"http://localhost:3000"];   //"http://localhost:3000"
+const app = express()
+const whitelist = ['https://www.gograbmoney.com', 'https://gograbmoney.com',"http://localhost:3000"];
 const corsOptions = {
     //origin: 'https://www.gograbmoney.com',  //https://www.gograbmoney.com & http://localhost:3000    
     credentials: true, // This is important.
     origin: (origin, callback) => {
-        if (whitelist.includes(origin)) {
+        if (whitelist.includes(origin))
             return callback(null, true)
-        }else{
-            return callback(new Error('Not allowed by CORS'));
-        }
+
+        callback(new Error('Not allowed by CORS'));
     }
 }
 
